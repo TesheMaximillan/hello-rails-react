3bfea90dca09e6eb94af8e651dd644fef8444bfb
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-08 22:25:27 +0300

    Configure routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :messages, only: [:index, :create]
+      get "/message/random", to: "messages#show"
+    end
+  end
+
+  get "*path", to: "pages#index", via: :all
 end
