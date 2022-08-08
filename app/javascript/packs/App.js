83cedcf2f8702f2200e9f0f43c0848abd5feba48
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-09 01:51:50 +0300

    Wrap App component with provider

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/configureStore'
+import Greeting from './components/Greeting'
+import { getRandomMessage } from './redux/reducer'
 
 const App = () => {
   return (
-    <div>Hello There</div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </Router>
+    </Provider>
   )
 }
 
